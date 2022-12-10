5618d9a5770383e9f0a1f4d8894b0f23a01553dd
up930506 <113272594+up930506@users.noreply.github.com>
2022-12-10 01:18:37 +0000

    Update automation

--- automation.py
+++ automation.py
@@ -3,15 +3,24 @@ import requests
 import json
 import os
 import tempfile
+import zipfile
 
 def api_req(endpoint):
   return requests.get('https://api.modrinth.com/v2/{0}'.format(endpoint), headers={
     'User-Agent': '1.18.2 plus+/v0.0.2'
   }).json()
 
+def listdir(directory):
+  items = []
+  for item in os.listdir(directory):
+    item_fpath = os.path.join(directory, item)
+    items.append(item_fpath)
+    if os.path.isdir(item_fpath):
+      for item_nested in listdir(item_fpath):
+        items.append(item_nested)
+  return items
+
 tmp_dir = tempfile.TemporaryDirectory()
-working_dir = os.path.join(tmp_dir.name, 'mods')
-os.mkdir(working_dir)
 with open('docs/mods.md', 'w') as fd0:
   fd0.write('# Mods\nIcon | Name | Version\n--- | --- | ---\n')
   for mod in os.listdir('mods'):
@@ -20,7 +29,6 @@ with open('docs/mods.md', 'w') as fd0:
       mod_json = json.loads(fd1.read())
     mod_project = api_req('project/{0}'.format(mod_json['project_id']))
     mod_version = api_req('version/{0}'.format(mod_json['version_id']))
-    #print(mod_version['files'])
     # Write docs
     fd0.write('<img src="{0}" width=64px height=64px> | [{1}](https://modrinth.com/project/{2}) | {3}\n'.format(
       mod_project['icon_url'],
@@ -28,9 +36,17 @@ with open('docs/mods.md', 'w') as fd0:
       mod_json['project_id'],
       mod_version['version_number']
     ))
+    # Download mod files
     for file in mod_version['files']:
       resp = requests.get(file['url'])
-      with open(os.path.join(working_dir, file['filename']), 'wb') as fd1:
+      tmp_loc = os.path.join(tmp_dir.name, file['filename'])
+      with open(tmp_loc, 'wb') as fd1:
         fd1.write(resp.content)
-print(os.listdir(working_dir))
+      with zipfile.ZipFile('modpack.zip', 'a') as fd1:
+        fd1.write(tmp_loc, os.path.join('mods', file['filename']))
+# Clean up tmp directory
 tmp_dir.cleanup()
+# Write extra files to archive
+with zipfile.ZipFile('modpack.zip', 'a') as fd0:
+  for item in listdir('include'):
+    fd0.write(item, item.split('include/', 1)[1])
