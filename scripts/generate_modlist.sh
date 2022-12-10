ce0688a272423126c7b83f4caff1cbbaf8bc813f
up930506 <113272594+up930506@users.noreply.github.com>
2022-12-10 15:21:13 +0000

    Update doc generation script

--- scripts/generate_modlist.sh
+++ scripts/generate_modlist.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cat << EOF > mods.md
+cat << EOF > docs/mods.md
 # Mods
 Name | Local file | Hash
 --- | --- | ---
 EOF
-for mod in ../modpack/mods/*.toml; do
+for mod in modpack/mods/*.toml; do
   mod=$(cat "${mod}")
   mod_name=$(echo "${mod}" | sed -nr "s/^name = \"(.*)\"$/\1/p")
   mod_filename=$(echo "${mod}" | sed -nr "s/^filename = \"(.*)\"$/\1/p")
   mod_hash_format=$(echo "${mod}" | sed -nr "s/^hash-format = \"(.*)\"$/\1/p")
   mod_hash=$(echo "${mod}" | sed -nr "s/^hash = \"(.*)\"$/\1/p")
-  echo "${mod_name} | ${mod_filename} | ${mod_hash} (${mod_hash_format})" >> mods.md
+  echo "${mod_name} | ${mod_filename} | ${mod_hash} (${mod_hash_format})" >> docs/mods.md
 done
